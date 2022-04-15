c7e901bd443f1c698a1ec22113e7e6f2619d6aa5
Tom Forbes <tom@tomforb.es>
2022-04-15 15:47:44 +0000

    {"name":"apachecn-oj-wiki","version":"2022.4.15.0","file":"apachecn-oj-wiki-2022.4.15.0.tar.gz","path":"apachecn-oj-wiki/apachecn-oj-wiki-2022.4.15.0.tar.gz"}

---
+++ apachecn-oj-wiki/apachecn-oj-wiki-2022.4.15.0.tar.gz/ApachecnOjWiki/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-oj-wiki
+https://github.com/apachecn/apachecn-oj-wiki"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.4.15.0"
